1c3e6b0169a85b713a7201ff3eaaa97eeae1138d
SelmanSaracoglu <selman.saracoglu@outlook.com>
2025-10-07 14:24:16 +0200

    feat: Implement BookService with initial business logic

---
+++ src/main/java/com/selman/bookstore/service/BookService.java
@@ -0,0 +1,38 @@
+package com.selman.bookstore.service;
+
+
+import com.selman.bookstore.domain.Book;
+import com.selman.bookstore.repository.BookRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    // Constructor Injection
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> findAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Optional<Book> findBookById(Long id) {
+        return bookRepository.findById(id);
+    }
+
+    public Book createBook(Book book) {
+        // Check if a book with the same ISBN already exists
+        Optional<Book> bookOptional = bookRepository.findByIsbn(book.getIsbn());
+        if (bookOptional.isPresent()) {
+            throw new IllegalStateException("A book with ISBN " + book.getIsbn() + " already exists.");
+        }
+        return bookRepository.save(book);
+    }
+}
