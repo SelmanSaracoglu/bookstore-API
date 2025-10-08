fae984fb5d370b63107f0a7d5084d310899c5afe
SelmanSaracoglu <selman.saracoglu@outlook.com>
2025-10-08 16:02:57 +0200

    feat: Implement user registration endpoint

---
+++ src/main/java/com/selman/bookstore/web/rest/AuthController.java
@@ -0,0 +1,28 @@
+package com.selman.bookstore.web.rest;
+
+
+import com.selman.bookstore.dto.RegisterRequestDTO;
+import com.selman.bookstore.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@Valid @RequestBody RegisterRequestDTO registerRequest){
+        authService.registerUser(registerRequest);
+        return ResponseEntity.ok("User registered successfully!");
+    }
+}
